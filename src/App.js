8a0b6951bad7415399eec0d766f00f5b62eeeff0
Arthurmtro <Arthurmtro@gmail.com>
2020-02-13 22:25:39 +0100

    adding ignore

--- src/App.js
+++ src/App.js
@@ -7,12 +7,7 @@ import Register from './components/register/Register';
 import Logo from './components/logo/Logo';
 import Rank from './components/rank/Rank';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import './App.css';
-
-const app = new Clarifai.App({
-   apiKey: '634355dfeb784cb382cbfb139ca96581'
-});
 
 const particlesOptions = {
    particles: {
@@ -26,17 +21,38 @@ const particlesOptions = {
    }
 };
 
+const initialSate = {
+   input: "",
+   imageUrl: '',
+   box: {},
+   route: 'signin',
+   isSignedIn: false,
+   user: {
+      id: '',
+      name: '',
+      email: '',
+      entries: 0,
+      joined: ''
+   }
+};
+
+
+
 class App extends Component {
    constructor() {
       super();
-      this.state = {
-         input: "",
-         imageUrl: '',
-         box: {},
-         route: 'signin',
-         isSignedIn: false
-      }
+      this.state = initialSate;
    }
+   
+   loadUser = (data) => {
+      this.setState({user: {
+            id: data.id,
+            name: data.name,
+            email: data.email,
+            entries: data.entries,
+            joined: data.joined
+         }})
+      }
    
    calculateFaceLocation = (data) => {
       const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -60,19 +76,39 @@ class App extends Component {
       this.setState({input: event.target.value});
    };
    
-   onButtonSubmit = () => {
+   onPictureSubmit = () => {
       this.setState({imageUrl: this.state.input});
-      app.models
-      .predict(
-         Clarifai.FACE_DETECT_MODEL,
-         this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      fetch('http://localhost:3200/imageurl', {
+         method: 'post',
+         headers: {'Content-Type': 'application/json'},
+         body: JSON.stringify({
+            input: this.state.input
+         })
+      })
+      .then(response => response.json())
+      .then(response => {
+         if (response) {
+            fetch('http://localhost:3200/image', {
+               method: 'put',
+               headers: {'Content-Type': 'application/json'},
+               body: JSON.stringify({
+                  id: this.state.user.id
+               })
+            })
+            .then(response => response.json())
+            .then(count => {
+               this.setState(Object.assign(this.state.user, { entries: count}))
+            })
+            .catch(console.log)
+         }
+         this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
    };
    
    onRouteChange = (route) => {
       if (route === 'signout') {
-         this.setState({isSignedIn: false})
+         this.setState(initialSate)
       } else if (route === 'home') {
          this.setState({isSignedIn: true})
       }
@@ -90,17 +126,17 @@ class App extends Component {
             {route === 'home'
                ? <div>
                   <Logo />
-                  < Rank / >
+                  <Rank name={this.state.user.name} entries={this.state.user.entries}/>
                   < ImageLinkForm
                      onInputChange={this.onInputChange}
-                     onButtonSubmit={this.onButtonSubmit}
+                     onButtonSubmit={this.onPictureSubmit}
                      />
                   <FaceRecognition box={box} imageUrl={imageUrl}/>
                 </div>
                : (
                   route === 'signin'
-                     ? <Signin onRouteChange={this.onRouteChange}/>
-                     : <Register onRouteChange={this.onRouteChange}/>
+                     ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+                     : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
                )
             }
          </div>
--- src/components/rank/Rank.js
+++ src/components/rank/Rank.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const Rank = () => {
+const Rank = ({name, entries}) => {
    return (
       <div>
-         <div className='white f3'>
-            {"'User', your current rank is ..."}
-         </div>
-         <div className='white f1'>
-            {"#?"}
+         {`${name} , your current entries count is...`}
+         <div className='white f1 '>
+            {entries}
          </div>
       </div>
    )
